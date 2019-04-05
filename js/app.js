fe67d9545602cddb3730cda16d6318d26c71cddd
Anil Keeramkarye <atkeeramkary@gmail.comm>
2019-04-05 16:44:59 -0500

    playing grid established

---
+++ js/app.js
@@ -0,0 +1,67 @@
+
+console.log("Hello World");
+// $(document).ready(function() {
+
+// 	console.log('working');
+// });
+
+
+// class C4Grid {
+// 	constructor(selector){
+// 		this.ROWS = 6;
+// 		this.COLS = 7;
+// 		this.selector = selector
+		
+// 	}
+// 	createGrid() {
+// 		const $board = $(this.selector);
+
+// 		console.log($board);
+// 		for (let row = 0; row < this.ROWS; row++){
+// 			const $row = $('<div>')
+// 			.addClass('row');
+// 			for (let col = 0; col < this.COLS; col++) {
+// 				const $col = $('<div>')
+// 				.addClass('col empty');
+// 				$row.append($col);	
+// 			}
+// 			$board.append($row); 
+// 		}
+
+		
+// 	}
+
+
+// } 
+
+// $(document).ready(function() {
+// const connect4 = new C4Grid('#connect4')
+
+// });
+// const game = {
+// 	grid: null,
+// }
+
+
+// for (let col = 0; col < this.COLS; col++){
+			
+// 		}	
+// // function makeBoard () => {
+	
+
+
+// 	for( let row = 0; row <= 5; row++) {
+		
+		
+
+// 		}
+
+// 	for( let col= 0; col <= 6; col++) {
+	
+
+// 	}
+
+// }
+
+/// listen for .square
+// see if you can print the id -- hint: use event.target
