8c592ce4deecbfc26f7b81606fc80337c3ff622c
Anil Keeramkarye <atkeeramkary@gmail.comm>
2019-04-09 13:06:05 -0500

    added vertical solutions

--- js/app.js
+++ js/app.js
@@ -674,9 +674,285 @@ const player1 = {
 			return true 
 		}
 
+		if (
+			this.slotsFilled.includes('sq-1-1') && 
+			this.slotsFilled.includes('sq-1-2') &&
+			this.slotsFilled.includes('sq-1-3') &&
+			this.slotsFilled.includes('sq-1-4') 
 
+		) {
+			return true 
+		}
 
+		if (
+			this.slotsFilled.includes('sq-1-2') && 
+			this.slotsFilled.includes('sq-1-3') &&
+			this.slotsFilled.includes('sq-1-4') &&
+			this.slotsFilled.includes('sq-1-5') 
 
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-1-3') && 
+			this.slotsFilled.includes('sq-1-4') &&
+			this.slotsFilled.includes('sq-1-5') &&
+			this.slotsFilled.includes('sq-1-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-1-4') && 
+			this.slotsFilled.includes('sq-1-5') &&
+			this.slotsFilled.includes('sq-1-6') &&
+			this.slotsFilled.includes('sq-1-7') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-2-1') && 
+			this.slotsFilled.includes('sq-2-2') &&
+			this.slotsFilled.includes('sq-2-3') &&
+			this.slotsFilled.includes('sq-2-4') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-2-2') && 
+			this.slotsFilled.includes('sq-2-3') &&
+			this.slotsFilled.includes('sq-2-4') &&
+			this.slotsFilled.includes('sq-2-5') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-2-3') && 
+			this.slotsFilled.includes('sq-2-4') &&
+			this.slotsFilled.includes('sq-2-5') &&
+			this.slotsFilled.includes('sq-2-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-2-4') && 
+			this.slotsFilled.includes('sq-2-5') &&
+			this.slotsFilled.includes('sq-2-6') &&
+			this.slotsFilled.includes('sq-2-7') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-3-1') && 
+			this.slotsFilled.includes('sq-3-2') &&
+			this.slotsFilled.includes('sq-3-3') &&
+			this.slotsFilled.includes('sq-3-4') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-3-2') && 
+			this.slotsFilled.includes('sq-3-3') &&
+			this.slotsFilled.includes('sq-3-4') &&
+			this.slotsFilled.includes('sq-3-5') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-3-3') && 
+			this.slotsFilled.includes('sq-3-4') &&
+			this.slotsFilled.includes('sq-3-5') &&
+			this.slotsFilled.includes('sq-3-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-3-4') && 
+			this.slotsFilled.includes('sq-3-5') &&
+			this.slotsFilled.includes('sq-3-6') &&
+			this.slotsFilled.includes('sq-3-7') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-4-1') && 
+			this.slotsFilled.includes('sq-4-2') &&
+			this.slotsFilled.includes('sq-4-3') &&
+			this.slotsFilled.includes('sq-4-4') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-4-2') && 
+			this.slotsFilled.includes('sq-4-3') &&
+			this.slotsFilled.includes('sq-4-4') &&
+			this.slotsFilled.includes('sq-4-5') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-4-3') && 
+			this.slotsFilled.includes('sq-4-4') &&
+			this.slotsFilled.includes('sq-4-5') &&
+			this.slotsFilled.includes('sq-4-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-4-4') && 
+			this.slotsFilled.includes('sq-4-5') &&
+			this.slotsFilled.includes('sq-4-6') &&
+			this.slotsFilled.includes('sq-4-7') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-5-1') && 
+			this.slotsFilled.includes('sq-5-2') &&
+			this.slotsFilled.includes('sq-5-3') &&
+			this.slotsFilled.includes('sq-5-4') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-5-2') && 
+			this.slotsFilled.includes('sq-5-3') &&
+			this.slotsFilled.includes('sq-5-4') &&
+			this.slotsFilled.includes('sq-5-5') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-5-3') && 
+			this.slotsFilled.includes('sq-5-4') &&
+			this.slotsFilled.includes('sq-5-5') &&
+			this.slotsFilled.includes('sq-5-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-5-4') && 
+			this.slotsFilled.includes('sq-5-5') &&
+			this.slotsFilled.includes('sq-5-6') &&
+			this.slotsFilled.includes('sq-5-7') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-6-1') && 
+			this.slotsFilled.includes('sq-6-2') &&
+			this.slotsFilled.includes('sq-6-3') &&
+			this.slotsFilled.includes('sq-6-4') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-6-2') && 
+			this.slotsFilled.includes('sq-6-3') &&
+			this.slotsFilled.includes('sq-6-4') &&
+			this.slotsFilled.includes('sq-6-5') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-6-3') && 
+			this.slotsFilled.includes('sq-6-4') &&
+			this.slotsFilled.includes('sq-6-5') &&
+			this.slotsFilled.includes('sq-6-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-6-4') && 
+			this.slotsFilled.includes('sq-6-5') &&
+			this.slotsFilled.includes('sq-6-6') &&
+			this.slotsFilled.includes('sq-6-7') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-7-1') && 
+			this.slotsFilled.includes('sq-7-2') &&
+			this.slotsFilled.includes('sq-7-3') &&
+			this.slotsFilled.includes('sq-7-4') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-7-2') && 
+			this.slotsFilled.includes('sq-7-3') &&
+			this.slotsFilled.includes('sq-7-4') &&
+			this.slotsFilled.includes('sq-7-5') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-7-3') && 
+			this.slotsFilled.includes('sq-7-4') &&
+			this.slotsFilled.includes('sq-7-5') &&
+			this.slotsFilled.includes('sq-7-6') 
+
+		) {
+			return true 
+		}
+
+		if (
+			this.slotsFilled.includes('sq-7-4') && 
+			this.slotsFilled.includes('sq-7-5') &&
+			this.slotsFilled.includes('sq-7-6') &&
+			this.slotsFilled.includes('sq-7-7') 
+
+		) {
+			return true 
+		}
 
 
 
--- js2/app2.js
+++ js2/app2.js
@@ -27,44 +27,44 @@
 
 vertical solutions:
 
-id='sq-1-1'   id='sq-1-2'   id='sq-1-3'  id='sq-1-4'   
-id='sq-1-2'   id='sq-1-3'   id='sq-1-4'  id='sq-1-5'   
-id='sq-1-3'   id='sq-1-4'   id='sq-1-5'  id='sq-1-6'   
-id='sq-1-4'   id='sq-1-5'   id='sq-1-6'  id='sq-1-7'   
+id='sq-1-1'   id='sq-1-2'   id='sq-1-3'  id='sq-1-4' -   
+id='sq-1-2'   id='sq-1-3'   id='sq-1-4'  id='sq-1-5' -  
+id='sq-1-3'   id='sq-1-4'   id='sq-1-5'  id='sq-1-6'  - 
+id='sq-1-4'   id='sq-1-5'   id='sq-1-6'  id='sq-1-7'  - 
    
    
-id='sq-2-1'   id='sq-2-2'   id='sq-2-3'  id='sq-2-4'
+id='sq-2-1'   id='sq-2-2'   id='sq-2-3'  id='sq-2-4' -
 
-id='sq-2-2'   id='sq-2-3'   id='sq-2-4'  id='sq-2-5'
+id='sq-2-2'   id='sq-2-3'   id='sq-2-4'  id='sq-2-5' -
 
-id='sq-2-3'   id='sq-2-4'   id='sq-2-5'  id='sq-2-6'
+id='sq-2-3'   id='sq-2-4'   id='sq-2-5'  id='sq-2-6' -
 
-id='sq-2-4'   id='sq-2-5'   id='sq-2-6'  id='sq-2-7'   
+id='sq-2-4'   id='sq-2-5'   id='sq-2-6'  id='sq-2-7' - 
 
-id='sq-3-1'   id='sq-3-2'   id='sq-3-3'  id='sq-3-4' 
-id='sq-3-2'   id='sq-3-3'   id='sq-3-4'  id='sq-3-5' 
-id='sq-3-3'   id='sq-3-4'   id='sq-3-5'  id='sq-3-6' 
-id='sq-3-4'   id='sq-3-5'   id='sq-3-6'  id='sq-3-7' 
+id='sq-3-1'   id='sq-3-2'   id='sq-3-3'  id='sq-3-4' -
+id='sq-3-2'   id='sq-3-3'   id='sq-3-4'  id='sq-3-5' -
+id='sq-3-3'   id='sq-3-4'   id='sq-3-5'  id='sq-3-6' -
+id='sq-3-4'   id='sq-3-5'   id='sq-3-6'  id='sq-3-7' -
 
-id='sq-4-1'   id='sq-4-2'   id='sq-4-3'  id='sq-4-4'  
-id='sq-4-2'   id='sq-4-3'   id='sq-4-4'  id='sq-4-5'  
-id='sq-4-3'   id='sq-4-4'   id='sq-4-5'  id='sq-4-6'  
-id='sq-4-4'   id='sq-4-5'   id='sq-4-6'  id='sq-4-7'  
+id='sq-4-1'   id='sq-4-2'   id='sq-4-3'  id='sq-4-4' - 
+id='sq-4-2'   id='sq-4-3'   id='sq-4-4'  id='sq-4-5' - 
+id='sq-4-3'   id='sq-4-4'   id='sq-4-5'  id='sq-4-6'  -
+id='sq-4-4'   id='sq-4-5'   id='sq-4-6'  id='sq-4-7'  -
 
-id='sq-5-1'   id='sq-5-2'   id='sq-5-3'  id='sq-5-4'  
-id='sq-5-2'   id='sq-5-3'   id='sq-5-4'  id='sq-5-5'  
-id='sq-5-3'   id='sq-5-4'   id='sq-5-5'  id='sq-5-6'  
-id='sq-5-4'   id='sq-5-5'   id='sq-5-6'  id='sq-5-7'  
+id='sq-5-1'   id='sq-5-2'   id='sq-5-3'  id='sq-5-4' - 
+id='sq-5-2'   id='sq-5-3'   id='sq-5-4'  id='sq-5-5' - 
+id='sq-5-3'   id='sq-5-4'   id='sq-5-5'  id='sq-5-6' - 
+id='sq-5-4'   id='sq-5-5'   id='sq-5-6'  id='sq-5-7' - 
 
-id='sq-6-1'   id='sq-6-2'   id='sq-6-3'  id='sq-6-4'  
-id='sq-6-2'   id='sq-6-3'   id='sq-6-4'  id='sq-6-5'  
-id='sq-6-3'   id='sq-6-4'   id='sq-6-5'  id='sq-6-6'  
-id='sq-6-4'   id='sq-6-5'   id='sq-6-6'  id='sq-6-7'  
+id='sq-6-1'   id='sq-6-2'   id='sq-6-3'  id='sq-6-4' - 
+id='sq-6-2'   id='sq-6-3'   id='sq-6-4'  id='sq-6-5'  -
+id='sq-6-3'   id='sq-6-4'   id='sq-6-5'  id='sq-6-6'  -
+id='sq-6-4'   id='sq-6-5'   id='sq-6-6'  id='sq-6-7'  -
 
-id='sq-7-1'   id='sq-7-2'   id='sq-7-3'  id='sq-7-4' 
-id='sq-7-2'   id='sq-7-3'   id='sq-7-4'  id='sq-7-5'  
-id='sq-7-3'   id='sq-7-4'   id='sq-7-5'  id='sq-7-6'  
-id='sq-7-4'   id='sq-7-5'   id='sq-7-6'  id='sq-7-7'  
+id='sq-7-1'   id='sq-7-2'   id='sq-7-3'  id='sq-7-4' -
+id='sq-7-2'   id='sq-7-3'   id='sq-7-4'  id='sq-7-5'  -
+id='sq-7-3'   id='sq-7-4'   id='sq-7-5'  id='sq-7-6'  -
+id='sq-7-4'   id='sq-7-5'   id='sq-7-6'  id='sq-7-7'  -
 
 
 
